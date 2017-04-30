118d759933a1ae40b26e1f4a96727a6597a226b5
haakensonb <untamedhawk44@gmail.com>
2017-04-30 00:46:53 -0400

    Landing page added, background cogs added

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $('.landing-area-container h1').mouseover(function(){
+        $('#arrow').addClass('arrow-hover');
+    });
+    $('.landing-area-container h1').mouseout(function(){
+        $('#arrow').removeClass('arrow-hover');
+    });
+
+    // smooth scrolling, implemented with the help of stack overflow
+    $(document).on('click', 'a[href^="#"]', function(e) {
+        // target element id
+        var id = $(this).attr('href');
+
+        // target element
+        var $id = $(id);
+        if ($id.length === 0) {
+            return;
+        }
+
+        // prevent standard hash navigation (avoid blinking in IE)
+        e.preventDefault();
+
+        // top position relative to the document
+        var pos = $(id).offset().top - 60;
+
+        // animated top scrolling
+        $('body, html').animate({scrollTop: pos});
+    });
+});
