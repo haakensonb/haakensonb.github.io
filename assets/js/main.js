c62967a656f21dd5fb6c40fc34a77581ed5ee92c
haakensonb <untamedhawk44@gmail.com>
2017-04-30 22:37:57 -0400

    More mobile breakpoints added, landing color changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
     $('.landing-area-container h1').mouseout(function(){
         $('#arrow').removeClass('arrow-hover');
     });
+    $('.landing-area-container h1').on('click', function(){
+        $('#arrow').removeClass('arrow-hover');
+    });
 
     // smooth scrolling, implemented with the help of stack overflow
     $(document).on('click', 'a[href^="#"]', function(e) {
@@ -26,4 +29,42 @@ $(document).ready(function(){
         // animated top scrolling
         $('body, html').animate({scrollTop: pos});
     });
+
+    /*
+ * Replace all SVG images with inline SVG
+ */
+    jQuery('img.svg').each(function(){
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+
+    });
+
 });
